cbfd387c85bea605ca8ae7ecd4c3a3cc3661e2d7
Shadowdx24 <110624444+Shadowdx24@users.noreply.github.com>
2023-09-25 18:43:53 +0530

    added new function in Script

--- Assets/Scripts/GamePlay.cs
+++ Assets/Scripts/GamePlay.cs
@@ -8,7 +8,9 @@ public class GamePlay : MonoBehaviour
 {
     private float n1;
     private float n2;
-    [SerializeField] private Button[] GridButtons; 
+    [SerializeField] private Button[] GridButtons;
+    private bool whoseTurn=false;
+    [SerializeField] private TextMeshProUGUI[] btnTexts;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,8 +25,71 @@ public class GamePlay : MonoBehaviour
 
     public void OnClickBtn(int num)
     {
-        GridButtons[num].GetComponentInChildren<TextMeshProUGUI>().text = "X";
-        Debug.Log("X");
+        if (whoseTurn == false) 
+        {
+            GridButtons[num].GetComponentInChildren<TextMeshProUGUI>().text = "O";
+            Debug.Log("X");
+            
+        }
+        else if (whoseTurn == true) 
+        {
+            GridButtons[num].GetComponentInChildren<TextMeshProUGUI>().text = "X";
+        }
+        changeTurn();
+        
+    }
+
+    private void changeTurn()
+    {
+        if(whoseTurn == false)
+        {
+            whoseTurn = true;
+        }
+        else if(whoseTurn==true) 
+        { 
+            whoseTurn = false;
+        }
+        Winsrcene();
+    }
+    private void Winsrcene()
+    {
+        if (btnTexts[0].text== btnTexts[1].text && btnTexts[0].text == btnTexts[2].text)
+        {
+            
+        }
+        else if (btnTexts[3].text == btnTexts[4].text && btnTexts[3].text == btnTexts[5].text)
+        {
+
+        }
+        else if (btnTexts[6].text == btnTexts[7].text && btnTexts[6].text == btnTexts[8].text)
+        {
+
+        }
+        //else if (btnTexts[0].text == btnTexts[3].text && btnTexts[0].text == btnTexts[6].text)
+        //{
+
+        //}
+        //else if (btnTexts[1].text == btnTexts[4].text && btnTexts[1].text == btnTexts[7].text)
+        //{
+
+        //}
+        //else if (btnTexts[2].text == btnTexts[5].text && btnTexts[2].text == btnTexts[8].text)
+        //{
+
+        //}
+        //else if (btnTexts[0].text == btnTexts[4].text && btnTexts[0].text == btnTexts[8].text)
+        //{
+
+        //}
+        //else if (btnTexts[2].text == btnTexts[4].text && btnTexts[2].text == btnTexts[6].text)
+        //{
+
+        //}
+        //else 
+        //{
+
+        //}
+
     }
 
 }
